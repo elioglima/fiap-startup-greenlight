e742235addfafa92a5efe44f83be99180bc55cab
elio.lima <macstudio@local>
2023-05-11 00:10:33 -0300

    ajust

--- client/greenlight-front/src/App.tsx
+++ client/greenlight-front/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Teste React sss</p>
+        <p>Teste de React deu certo?</p>
       </header>
     </div>
   );
